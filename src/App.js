fc84877b9a82cb276fe3247103c82fc7427f55d1
makennawold <makennawold@gmail.com>
2023-04-17 22:12:13 -0500

    prevents extra clicks

--- src/App.js
+++ src/App.js
@@ -22,18 +22,18 @@ import tangled from "./images/card-backs/tangled.jpeg";
 
 function App() {
   let tangledArray = [
-    { num: 1, img: tangled1 },
-    { num: 2, img: tangled2 },
-    { num: 3, img: tangled3 },
-    { num: 4, img: tangled4 },
-    { num: 5, img: tangled5 },
-    { num: 6, img: tangled6 },
-    { num: 7, img: tangled7 },
-    { num: 8, img: tangled8 },
-    { num: 9, img: tangled9 },
-    { num: 10, img: tangled10 },
-    { num: 11, img: tangled11 },
-    { num: 12, img: tangled12 },
+    { num: 1, img: tangled1, isMatched: false },
+    { num: 2, img: tangled2, isMatched: false },
+    { num: 3, img: tangled3, isMatched: false },
+    { num: 4, img: tangled4, isMatched: false },
+    { num: 5, img: tangled5, isMatched: false },
+    { num: 6, img: tangled6, isMatched: false },
+    { num: 7, img: tangled7, isMatched: false },
+    { num: 8, img: tangled8, isMatched: false },
+    { num: 9, img: tangled9, isMatched: false },
+    { num: 10, img: tangled10, isMatched: false },
+    { num: 11, img: tangled11, isMatched: false },
+    { num: 12, img: tangled12, isMatched: false },
   ];
   let crittersArray = [
     { num: 1, img: "images/critters_images/critters_1.jpeg" },
@@ -52,7 +52,8 @@ function App() {
   const [cards, setCards] = useState([]);
   const [player1Points, setPlayer1Points] = useState(0);
   const [player2Points, setPlayer2Points] = useState(0);
-  const [cardOne, setCardOne] = useState(null);
+  const [selectedCards, setSelectedCards] = useState([]);
+  const [activePlayer, setActivePlayer] = useState(true);
 
   const shuffleCards = () => {
     let shuffledCards = [...tangledArray, ...tangledArray]
@@ -62,17 +63,54 @@ function App() {
     setCards(shuffledCards);
   };
 
+  const addPoint = () => {
+    if (activePlayer === true) {
+      setPlayer1Points(player1Points + 1);
+    } else {
+      setPlayer2Points(player2Points + 1);
+    }
+  };
+
   useEffect(() => {
     shuffleCards();
   }, []);
+
+  useEffect(() => {
+    console.log(selectedCards);
+    if (selectedCards.length == 2) {
+      setTimeout(() => {
+        checkMatch();
+      }, 1000);
+    }
+  }, [selectedCards]);
+
+  const checkMatch = () => {
+    if (selectedCards[0].num === selectedCards[1].num) {
+      console.log("its a match");
+      let updatedCards = cards.map((card) => {
+        if (card.num === selectedCards[0].num) {
+          return { ...card, isMatched: true };
+        }
+        return card;
+      });
+      addPoint();
+      setCards(updatedCards);
+    } else {
+      console.log("sadness");
+      setActivePlayer(!activePlayer);
+    }
+    setSelectedCards([]);
+  };
 
   return (
     <div className="app">
       <div className="header">
         <div className="player-container">
-          <div className="player">player 1 </div>
+          <div className={activePlayer ? "player active" : "player"}>
+            player 1
+          </div>
           <div className="points-container">
-            <div className="crown">
+            <div className={activePlayer ? "crown active" : "crown"}>
               <TbCrown />
             </div>
             <div className="points">{player1Points}</div>
@@ -84,9 +122,11 @@ function App() {
           <span className="title-medium">R</span>Y
         </div>
         <div className="player-container">
-          <div className="player">player 2 </div>
+          <div className={activePlayer ? "player" : "player active"}>
+            player 2
+          </div>
           <div className="points-container">
-            <div className="crown">
+            <div className={activePlayer ? "crown" : "crown active"}>
               <TbCrown />
             </div>
             <div className="points">{player2Points}</div>
@@ -96,7 +136,13 @@ function App() {
       <div className="card-container">
         <div className="cards">
           {cards.map((card) => (
-            <Card key={card.id} card={card} cardsTheme={cardsTheme} />
+            <Card
+              key={card.id}
+              card={card}
+              cardsTheme={cardsTheme}
+              setSelectedCards={setSelectedCards}
+              selectedCards={selectedCards}
+            />
           ))}
         </div>
       </div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,28 @@
 import React from "react";
 import { useEffect, useState } from "react";
 
-const Card = ({ card, cardsTheme }) => {
+const Card = ({ card, cardsTheme, setSelectedCards, selectedCards }) => {
   const [cardUp, setCardUp] = useState(false);
 
+  const handleClick = () => {
+    if (selectedCards.length < 2) {
+      setSelectedCards([...selectedCards, card]);
+      setCardUp(true);
+    }
+  };
+
+  useEffect(() => {
+    if (selectedCards.length == 0) {
+      setTimeout(() => {
+        setCardUp(false);
+      }, 1000);
+    }
+  }, [selectedCards]);
   return (
-    <div
-      className="card"
-      onClick={() => {
-        setCardUp(!cardUp);
-      }}
-    >
-      {cardUp ? (
+    <div className="card" onClick={handleClick}>
+      {card.isMatched ? (
+        <div className="matched"></div>
+      ) : cardUp ? (
         <div className="front">
           <img src={card.img} />
         </div>
